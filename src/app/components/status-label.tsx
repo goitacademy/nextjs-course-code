88013b46676bf299bdd739ee181ac14eb00e10f7
Serhiy Lovchinsky <s.lovchinsky@goit.ua>
2024-01-11 14:34:46 +0200

    Rework status label using clsx

--- src/app/components/status-label.tsx
+++ src/app/components/status-label.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import clsx from 'clsx';
 
 export enum Status {
   Active = 'active',
@@ -15,13 +16,13 @@ export interface StatusLabelProps {
 export default function StatusLabel({ children, status }: StatusLabelProps) {
   return (
     <div
-      className={`inline-flex items-center py-1 px-3.5 rounded-3xl text-sm font-medium ${
-        (status === Status.Active && 'text-green-700 bg-green-100') ||
-        (status === Status.NotActive && 'text-red-700 bg-red-100') ||
-        (status === Status.Pending && 'text-orange-700 bg-orange-100') ||
-        (status === Status.Suspended && 'text-blue-700 bg-blue-100') ||
-        ''
-      }`}
+      className={clsx(
+        'inline-flex items-center py-1 px-3.5 rounded-3xl text-sm font-medium',
+        status === Status.Active && 'text-green-700 bg-green-100',
+        status === Status.NotActive && 'text-red-700 bg-red-100',
+        status === Status.Pending && 'text-orange-700 bg-orange-100',
+        status === Status.Suspended && 'text-blue-700 bg-blue-100',
+      )}
     >
       <div className="w-1 h-1 mr-2 rounded-full bg-current" />
       {children}
