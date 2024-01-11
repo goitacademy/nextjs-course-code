612057ce2c76b978250bcd49f4ef9d60e061c643
Serhiy Lovchinsky <s.lovchinsky@goit.ua>
2024-01-11 14:16:31 +0200

    Install tailwind css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import NotActiveLabel from '@/app/components/not-active-label';
 export default function Home() {
   return (
     <main>
-      <h1>Home page</h1>
+      <h1 className="text-xl">Home page</h1>
       <ActiveLabel>Active</ActiveLabel>
       <NotActiveLabel>Not Active</NotActiveLabel>
     </main>
