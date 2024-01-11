4d2f6289d0b4fc39cac37917eed811643c503745
Serhiy Lovchinsky <s.lovchinsky@goit.ua>
2024-01-11 15:30:58 +0200

    Add link and hooks for navigation

--- src/app/components/sidebar-item.tsx
+++ src/app/components/sidebar-item.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import Image from 'next/image';
+import Link from 'next/link';
+import clsx from 'clsx';
 
 export interface SidebarItemProps {
+  current?: boolean;
   pathname: string;
   src: string;
   alt: string;
@@ -9,6 +12,7 @@ export interface SidebarItemProps {
 }
 
 export default function SidebarItem({
+  current,
   pathname,
   src,
   alt,
@@ -16,10 +20,17 @@ export default function SidebarItem({
 }: SidebarItemProps) {
   return (
     <li>
-      <a href={pathname} className="flex items-center h-9 mx-1 gap-3.5">
+      <Link
+        href={pathname}
+        className={clsx(
+          'flex items-center h-9 mx-1 gap-3.5',
+          current &&
+            'after:h-full after:ml-auto after:border-2 after:border-purple-200 after:rounded-sm',
+        )}
+      >
         <Image className="ml-5" width={18} height={18} src={src} alt={alt} />
         <span className="font-medium text-zinc-50">{children}</span>
-      </a>
+      </Link>
     </li>
   );
 }
--- src/app/components/sidebar.tsx
+++ src/app/components/sidebar.tsx
@@ -1,10 +1,20 @@
+'use client';
+
 import React from 'react';
 import Image from 'next/image';
 import SidebarItem from '@/app/components/sidebar-item';
+import { usePathname, useRouter } from 'next/navigation';
 
 export interface SidebarProps {}
 
 export default function Sidebar({}: SidebarProps) {
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const handleExitClick = () => {
+    router.push('/');
+  };
+
   return (
     <aside className="fixed top-0 left-0 z-40 w-60 h-screen">
       <div className="flex flex-col h-full overflow-y-auto bg-gray-900">
@@ -17,6 +27,7 @@ export default function Sidebar({}: SidebarProps) {
         />
         <ul className="space-y-7">
           <SidebarItem
+            current={pathname === '/dashboard'}
             pathname="/dashboard"
             src="/icons/squares.svg"
             alt="dashboard icon"
@@ -24,6 +35,7 @@ export default function Sidebar({}: SidebarProps) {
             Dashboard
           </SidebarItem>
           <SidebarItem
+            current={pathname === '/companies'}
             pathname="/companies"
             src="/icons/briefcase.svg"
             alt="companies icon"
@@ -31,7 +43,10 @@ export default function Sidebar({}: SidebarProps) {
             Companies
           </SidebarItem>
         </ul>
-        <button className="flex items-center gap-2 p-6 mt-auto mx-auto">
+        <button
+          className="flex items-center gap-2 p-6 mt-auto mx-auto"
+          onClick={handleExitClick}
+        >
           <Image
             width={18}
             height={18}
