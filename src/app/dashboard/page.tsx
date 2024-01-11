989abbf123a0229229c91bdbb71e8aee8c3130f2
Serhiy Lovchinsky <s.lovchinsky@goit.ua>
2024-01-11 15:22:17 +0200

    Add dashboard page

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export interface PageProps {}
+
+export default function Page({}: PageProps) {
+  return (
+    <main>
+      <h1 className="text-xl">Dashboard Page</h1>
+    </main>
+  );
+}
